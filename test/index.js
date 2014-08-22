f0fe07f2e3c6677d2777f20cfda7c3e9449c2e92
Kyle Arrington <kyle@arrington.co>
2014-08-22 18:29:37 -0500

    Mocks added for json output, tests passing

---
+++ test/index.js
@@ -0,0 +1,42 @@
+var test = require('tape');
+var sword = require('sword');
+var veritas = require('../src/veritas');
+var source = require('../data/kjv.json');
+
+// Mocks
+// single verse
+var genc1v1 = require('./mock/gen-c1v1.json');
+// single chapter
+var genc1 = require('./mock/gen-c1.json');
+// verse range inside chapter
+var genc1v1c1v2 = require('./mock/gen-c1v1-c1v2.json');
+// verse range spans chapters
+var genc1v1c2v1 = require('./mock/gen-c1v1-c2v1.json');
+
+var verse = sword('Gen 1:1');
+
+test('sword returns expected values', function(t) {
+	t.deepEqual(
+		sword('Gen 1:1'), 
+		[{"start":{"book":1,"chapter":1,"verse":1}}]
+	);
+	t.end();
+});
+test('veritas returns a single verse', function (t) {
+	t.deepEqual(veritas([{"start":{"book":1,"chapter":1,"verse":1}}], source), genc1v1);
+	t.end();
+});
+test('veritas returns a single chapter', function (t) {
+	t.deepEqual(veritas([{"start":{"book":1,"chapter":1}}], source), genc1);
+	t.end();
+});
+test('veritas returns a range inside a chapter', function (t) {
+	t.deepEqual(
+		veritas([{"start":{"book":1,"chapter":1,"verse":1},"end":{"verse":2}}], source), genc1v1c1v2);
+	t.end();
+});
+test('veritas returns a range spanning chapters', function (t) {
+	t.deepEqual(
+		veritas([{"start":{"book":1,"chapter":1,"verse":1},"end":{"chapter":2,"verse":1}}], source), genc1v1c2v1);
+	t.end();
+});
